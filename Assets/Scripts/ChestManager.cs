4af092441aca4d28c6f62fdf0216b7e0e97f9d55
Alexander Wohlfahrt <akawohlfahrt@gmail.com>
2020-04-19 00:21:41 +0200

    Spawn the chest on the map, spawn the player near chest

---
+++ Assets/Scripts/ChestManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestManager : MonoBehaviour
+{
+    [SerializeField] private Transform chestSpawnersParent;
+    [SerializeField] private GameObject chestModel;
+    [SerializeField] private GameObject player;
+
+    private void Awake()
+    {
+        List<Transform> availableSpawners = new List<Transform>();
+        for (int i = 0; i < chestSpawnersParent.childCount; i++)
+        {
+            availableSpawners.Add(chestSpawnersParent.GetChild(i));
+        }
+
+        int rand = Random.Range(0, availableSpawners.Count - 1);
+        print(rand);
+
+        Transform spawner = availableSpawners[rand];
+
+        GameObject chest = Instantiate(chestModel);
+        chest.transform.localPosition = spawner.localPosition;
+        chest.transform.localScale = Vector3.one;
+
+        player.transform.localPosition = chest.transform.localPosition + Vector3.right * 0.5f;
+    }
+}
